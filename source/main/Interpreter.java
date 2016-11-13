a3cf514903dd57397fb59c8705551e95a35f7b64
Goncalao <goncalo.cool@gmail.com>
2016-11-13 19:14:10 +0000

    Adicao minimalista de codigo

---
+++ source/main/Interpreter.java
@@ -0,0 +1,29 @@
+public class Interpreter
+{
+	AppIO _app;
+	List<Program> _programs;
+
+	public Interpreter(AppIO app) {
+		_app = app;
+	}
+
+	public void setIdentifierValue(Identifier id, Literal value) {
+		
+	}
+
+	public Literal getIdentifierValue(Identifier id) {
+
+	}
+
+	public void addProgram(Program) {
+		
+	}
+
+	public Program getProgram(String name) {
+
+	}
+
+	public AppIO getAppIO() {
+		return app;
+	}
+}
